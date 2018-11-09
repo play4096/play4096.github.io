7c43eb724edcde4385616b46facbd670eea22d54
Secretwolf98 <jordanluders@gmail.com>
2018-11-09 13:03:40 -0600

    Update

--- js/application.js
+++ js/application.js
@@ -1,4 +1,22 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+
+  // TODO: This code is in need of a refactor (along with the rest)
+  var storage     = new LocalStorageManager;
+  var noticeClose = document.querySelector(".notice-close-button");
+  var notice      = document.querySelector(".app-notice");
+  if (storage.getNoticeClosed()) {
+    notice.parentNode.removeChild(notice);
+  } else {
+    noticeClose.addEventListener("click", function () {
+      notice.parentNode.removeChild(notice);
+      storage.setNoticeClosed(true);
+      if (typeof gtag !== undefined){
+        gtag("event", "closed", {
+          event_category: "notice",
+        });
+      }
+    });
+  }
 });
